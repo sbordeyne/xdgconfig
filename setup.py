dc5122733cfb9111c709003c0bf32048b2ac9c1c
Dogeek <Dogeek@users.noreply.github.com>
2021-02-06 20:06:58 +0100

    [python] Setup script

---
+++ setup.py
@@ -0,0 +1,64 @@
+import itertools
+
+from setuptools import setup, find_packages
+from pyconfig import __version__
+
+
+long_desc = ''
+requirements = []
+with open('readme.md') as f:
+    long_desc = f.read()
+
+
+extras = {
+    'jsonc': ['commentjson>=0.9.0,<1'],
+    'toml': ['toml>=0.10.2,<1'],
+    'yaml': ['PyYAML>=5.4.1,<6'],
+}
+
+for k in range(2, len(extras) + 1):
+    for perm in itertools.permutations(
+        [x for x in extras if '+' not in x], k
+    ):
+        extras['+'.join(perm)] = [
+            item
+            for sublist in [extras[p] for p in perm]
+            for item in set(sublist)
+        ]
+
+extras['all'] = [
+    item
+    for ex, sublist in extras.items()
+    if '+' not in ex
+    for item in set(sublist)
+]
+
+setup(
+    name='pyconfig',
+    version=__version__,
+    author='Dogeek',
+    author_email='dogeek@users-noreply.github.com',
+    url='https://github.com/dogeek/pyconfig',
+    description='Easy access to `~/.config`',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+    ],
+    packages=find_packages(),
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=True,
+    platforms='any',
+    python_requires='>=3.8',
+    download_url='https://github.com/dogeek/pyconfig/releases',
+    extras_require=extras,
+)
